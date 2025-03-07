6d017b88cce4c345a2bf4a820ab92cd43966616e
Ashley7790 <smashley314@gmail.com>
2025-03-07 10:41:32 -0500

    Update University.py

--- University.py
+++ University.py
@@ -27,11 +27,11 @@ df['YearTerm'] = df['Year'] + ' ' + df['Term']
 
 # Sidebar Filter
 st.sidebar.header("Filters")
-term_filter = st.sidebar.selectbox("Select Year-Term", ['All'] + list(df['YearTerm'].unique()))
+term_filter = st.sidebar.selectbox("Select Year", ['All'] + list(df['Year'].unique()))
 term_filter2 = st.sidebar.selectbox("Select Term", ['All'] + list(df['Term'].unique())) # Added Term filter
 
 if term_filter != 'All':
-    df = df[df['YearTerm'] == term_filter]
+    df = df[df['Year'] == term_filter]
 if term_filter2 != 'All':
     df = df[df['Term'] == term_filter2]
 
