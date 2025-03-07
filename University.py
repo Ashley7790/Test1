a22e80dc38a3e2f75a165746de0ae814e71a3488
Ashley7790 <smashley314@gmail.com>
2025-03-07 10:21:48 -0500

    Update University.py

--- University.py
+++ University.py
@@ -25,6 +25,7 @@ if term_filter != 'All':
     df = df[df['Year'] == term_filter]
 
 # KPIs
+col1, col2, col3 = st.columns(3)
 st.metric("Total Applications", df['Applications'].sum())
 st.metric("Total Admitted", df['Admitted'].sum())
 st.metric("Total Enrolled", df['Enrolled'].sum())
@@ -49,6 +50,10 @@ if total_enrolled != 0:
   st.metric("Percent Enrolled", f"{percent_enrolled:.2f}%")
 else:
   st.metric("Percent Enrolled", "N/A")
+
+col1.metric("Total Applications", total_applications)
+col2.metric("Total Admitted", total_admitted)
+col3.metric("Total Enrolled", total_enrolled)
 
 
 # Chart
