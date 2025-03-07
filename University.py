e551f9e17277ee16231f42d6268efba7e1b9c2a3
Ashley7790 <smashley314@gmail.com>
2025-03-07 10:17:25 -0500

    Update University.py

--- University.py
+++ University.py
@@ -37,6 +37,8 @@ if total_applications > 0:
 else:
   percent_admitted = 0  
 
+# Display the metric
+st.metric("Percent Admitted", f"{percent_admitted:.2f}%")
 
 # Calculate the percentage enrolled
 total_admitted = df['Admitted'].sum()
