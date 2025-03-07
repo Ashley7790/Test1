510fc92a887ba98ff4868236e724bcde751928ea
Ashley7790 <smashley314@gmail.com>
2025-03-07 07:43:29 -0500

    Update University.py

--- University.py
+++ University.py
@@ -2,7 +2,7 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib as plt
 
 # Load Data
 df = pd.read_csv('university_student_dashboard_data.csv')
